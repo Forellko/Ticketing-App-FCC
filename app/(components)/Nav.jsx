4f9228d1520f46a14d504c995f7ccee49aadac9d
forellko <dragonforce97q@gmail.com>
2023-10-21 22:14:02 +0300

    Add nav links

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,5 +1,23 @@
+import { faHome, faTicket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div>
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div>
+        <p className="text-default-text">forellko</p>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
