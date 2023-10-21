7e26196c5068b05ed5bec2f86b5f75274e39bbc1
forellko <dragonforce97q@gmail.com>
2023-10-21 23:56:22 +0300

    Add flex to nav

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 const Nav = () => {
   return (
-    <nav>
-      <div>
+    <nav className="flex justify-between bg-nav p-4">
+      <div className="flex items-center space-x-4">
         <Link href="/">
           <FontAwesomeIcon icon={faHome} className="icon" />
         </Link>
