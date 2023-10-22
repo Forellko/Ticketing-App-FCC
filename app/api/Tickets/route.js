231363d8852514be24ec5d518f14b730ecf1a0ab
forellko <dragonforce97q@gmail.com>
2023-10-22 23:55:45 +0300

    Add get tickets from mongodb

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -7,6 +7,13 @@ export async function POST(req) {
     const ticketData = body.formData;
     await Ticket.create(ticketData);
     return NextResponse.json({ message: 'Ticket created' }, { status: 201 });
+  } catch (error) {}
+}
+
+export async function GET() {
+  try {
+    const tickets = await Ticket.find();
+    return NextResponse.json({ tickets }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: 'Error', error }, { status: 500 });
   }
